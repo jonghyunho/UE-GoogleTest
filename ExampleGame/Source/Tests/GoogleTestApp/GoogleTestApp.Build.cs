29eca9dd94756b6cc9bf6a2e1744e110b104f1ae
Jonghyun Ho <jonghyun.ho@gmail.com>
2019-10-15 02:26:16 +0900

    Add ExampleGameCore

--- ExampleGame/Source/Tests/GoogleTestApp/GoogleTestApp.Build.cs
+++ ExampleGame/Source/Tests/GoogleTestApp/GoogleTestApp.Build.cs
@@ -6,19 +6,11 @@ namespace UnrealBuildTool.Rules {
             PrivateDependencyModuleNames.AddRange(
                 new string[]
                 {
-                    "GoogleTest"
+                    "GoogleTest",
+                    "ExampleGameCore"
                 }
             );
             PrivatePCHHeaderFile = "Private/GoogleTestApp.h";
-
-/*
-            PublicIncludePaths.Add("ThirdParty/GoogleTest/googletest-release-1.10.0/googletest");
-            PublicIncludePaths.Add("ThirdParty/GoogleTest/googletest-release-1.10.0/googletest/include");
-            PublicIncludePaths.Add("ThirdParty/GoogleTest/googletest-release-1.10.0/googletest/src");
-            PublicIncludePaths.Add("ThirdParty/GoogleTest/googletest-release-1.10.0/googlemock");
-            PublicIncludePaths.Add("ThirdParty/GoogleTest/googletest-release-1.10.0/googlemock/include");
-            PublicIncludePaths.Add("ThirdParty/GoogleTest/googletest-release-1.10.0/googlemock/src");
-*/
         }
     }
 }

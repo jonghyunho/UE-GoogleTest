b91a577e2176d6357f9ead813a727e2d74b11af1
Jonghyun Ho <jonghyun.ho@gmail.com>
2019-10-15 01:36:30 +0900

    Clean up project

---
+++ ExampleGame/Clean.cmd
@@ -0,0 +1,5 @@
+del /Q /F /S .\Binaries\Win32
+del /Q /F /S .\Binaries\Win64
+del /Q /F /S .\Intermediate\*
+del /Q /F /S .\Saved\Logs\*.log
+pause
--- ExampleGame/Intermediate/Build/Win32/GoogleTestApp/Development/GoogleTestApp/PCH.GoogleTestApp.cpp
+++
@@ -1,1 +0,0 @@
-#include "D:/git/ExampleGame/Intermediate/Build/Win32/GoogleTestApp/Development/GoogleTestApp/PCH.GoogleTestApp.h"

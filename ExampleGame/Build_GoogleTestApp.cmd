791beff542bbd78005df8ca9f1a5e868916d4430
Jonghyun Ho <jonghyun.ho@gmail.com>
2019-10-15 02:40:40 +0900

    Add Build_GoogleTestApp.cmd

---
+++ ExampleGame/Build_GoogleTestApp.cmd
@@ -0,0 +1,4 @@
+"C:\Program Files\Epic Games\UE_4.22\Engine\Build\BatchFiles\Build.bat" GoogleTestApp Win64 Development -Project="D:\git\UE4-GoogleTest\ExampleGame\GoogleTestApp.uproject" -WaitMutex -FromMsBuild"
+
+@echo off
+pause
